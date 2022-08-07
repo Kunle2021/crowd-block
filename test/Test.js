d4a916ead5994bf49050a74d368e364995eb6533
Kunle2021 <kunle.elerewe@gmail.com>
2022-08-07 14:12:50 +0100

    All Test have passed

--- test/Test.js
+++ test/Test.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { provider } = require("ganache");
+const { assert } = require("console");
 
 describe("Test Project Contract functions", function () {
   async function deployContract() {
@@ -23,35 +24,51 @@ describe("Test Project Contract functions", function () {
   it("Contribute to a campaign", async () => {
     const { hardhatProject, owner } = await loadFixture(deployContract);
 
-    const sendEth = await hardhatProject.connect(owner).Contribute({
-      value: ethers.utils.parseEther("1.0"),
+    const provider = waffle.provider;
+
+    expect(await provider.getBalance(hardhatProject.address)).to.equal(0);
+    console.log("contract balance is zero");
+
+    await hardhatProject.Contribute({
+      value: 100,
+      from: owner.address,
     });
-    await sendEth.wait();
 
-    //check the contract balance
-    const provider = ethers.getDefaultProvider();
-
-    console.log(hardhatProject.address);
-    const balance = await provider.getBalance(hardhatProject.address);
-    const eth = ethers.utils.formatEther(balance);
-    console.log(eth);
-
-    expect(Math.round(eth)).to.equal(43);
+    expect(await provider.getBalance(owner.address)).to.equal(100);
+    console.log("Account Balance is 100");
   });
 
   it("Has the contribtion been approved, should be +1", async () => {
     const { hardhatProject, owner } = await loadFixture(deployContract);
 
-    const sendEth = await hardhatProject.connect(owner).Contribute({
-      value: ethers.utils.parseEther("1.0"),
+    await hardhatProject.Contribute({
+      value: 100,
+      from: owner.address,
     });
-    await sendEth.wait();
 
     const value = await hardhatProject.contributersCount();
-    console.log(value);
+    console.log("Contributers: " + value);
     expect(value).to.equal(1);
+  });
 
-    //Increased contributer
+  it("Require a minimum contribution", async () => {
+    const { hardhatProject, owner } = await loadFixture(deployContract);
+
+    const provider = waffle.provider;
+
+    try {
+      await hardhatProject.Contribute({
+        value: 9,
+        from: owner.address,
+      });
+
+      let con = await provider.getBalance(owner.address);
+      console.log("contribution is met: ");
+    } catch (error) {
+      con = await provider.getBalance(owner.address);
+      console.log("min contribution is not met: ");
+      assert(error);
+    }
   });
 
   //Need to test the Request functions tommorow
